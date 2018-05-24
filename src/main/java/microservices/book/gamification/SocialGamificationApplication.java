15e9cc6a0fa3c9d0222e8c86abc1813b7bfd779c
Mobilnik <iaulitin@yandex.ru>
2018-05-24 13:49:15 +0300

    Hystrix Configuration.

--- src/main/java/microservices/book/gamification/SocialGamificationApplication.java
+++ src/main/java/microservices/book/gamification/SocialGamificationApplication.java
@@ -2,13 +2,14 @@ package microservices.book.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class SocialGamificationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SocialGamificationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialGamificationApplication.class, args);
+    }
 }
